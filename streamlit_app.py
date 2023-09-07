32947b11db34ceda83994a5b4ecb894fc3c8689f
antony best <114415829+ant-capgemini@users.noreply.github.com>
2023-09-07 16:58:45 +0100

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,12 @@ My_Fruit_List = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 
 streamlit.dataframe(My_Fruit_List)
 
-My_Fruit_List = My_Fruit_Listt.set_index('Fruit')
+My_Fruit_List = My_Fruit_List.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(My_Fruit_List.index))
 
 # Display the table on the page.
 streamlit.dataframe(My_Fruit_List)
+
+
