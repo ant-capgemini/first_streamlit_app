a5a4d6b7c6aa12c54c4d015ebe089ec0a9c9b36b
antony best <114415829+ant-capgemini@users.noreply.github.com>
2023-09-07 16:49:29 +0100

    Update streamlit_app.py
    Added CSV file data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("My parents new healthy dinner")
 
@@ -10,3 +11,7 @@ streamlit.text("🐔 - Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 - Advocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+My_Fruit_List = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(My_Fruit_List)
