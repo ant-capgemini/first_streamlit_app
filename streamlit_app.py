f622b0654470ed4258fccb84073797ac1ebde5fd
antony best <114415829+ant-capgemini@users.noreply.github.com>
2023-09-08 10:49:03 +0100

    Update streamlit_app.py
    fixed loc brackets

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ My_Fruit_List = My_Fruit_List.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 Fruits_Selected = streamlit.multiselect("Pick some fruits:", list(My_Fruit_List.index), ['Avocado', 'Strawberries', 'Apple'])
-Fruits_To_Show = My_Fruit_List.loc(Fruits_Selected)
+Fruits_To_Show = My_Fruit_List.loc[Fruits_Selected]
 
 # Display the table on the page.
 streamlit.dataframe(Fruits_To_Show)
